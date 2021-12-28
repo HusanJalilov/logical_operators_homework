2bd8058019824144d93779876dcdcf396afe1ed4
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-28 23:58:14 +0500

    Task 17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,13 @@ def main(a):
     Returns:
         True if all digits of a are in ascending order, False otherwise
     """
-    return
+    x1=a%10
+    a//=10
+    x2=a%10
+    a//=10
+    x3=a%10
+    a//=10
+    x4=a%10
+    a//=10
+    x5=a%10
+    return x1>x2 and x2>x3 and x3>x4 and x4>x5
