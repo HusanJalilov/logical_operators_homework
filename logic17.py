cea512e4df80b85558894c38def59b37743b0090
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-29 00:02:19 +0500

    Task 18

--- logic17.py
+++ logic17.py
@@ -15,4 +15,4 @@ def main(a):
     x4=a%10
     a//=10
     x5=a%10
-    return x1>x2 and x2>x3 and x3>x4 and x4>x5
+    return x1<x2 and x2<x3 and x3<x4 and x4<x5
--- logic18.py
+++ logic18.py
@@ -5,4 +5,14 @@ def main(a):
     Returns:
         True if all digits of a are in descending order, False otherwise
     """
+    x1=a%10
+    a//=10
+    x2=a%10
+    a//=10
+    x3=a%10
+    a//=10
+    x4=a%10
+    a//=10
+    x5=a%10
+    return x1>x2 and x2>x3 and x3>x4 and x4>x5
     return
