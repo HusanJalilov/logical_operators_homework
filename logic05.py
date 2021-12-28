277f2351dca900423e76f58e9f352a606ea7ebc1
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-28 23:35:41 +0500

    Task 05

--- logic05.py
+++ logic05.py
@@ -1,10 +1,10 @@
 def main(a,b):
     """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is odd".
-    Args:
-        a: int
-        b: int
-    Returns:
-        True if each of the numbers 'a' and 'b' is odd, False otherwise
-    """
-    return
+        Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is odd".
+        Args:
+            a: int
+            b: int
+        Returns:
+            True if each of the numbers 'a' and 'b' is odd, False otherwise
+        """
+    return a%2!=0 and b%2!=0
